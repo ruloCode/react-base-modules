f72e7109180f006bb0a368dcd55b92ebf7841967
Andrés <drew.lizcano@gmail.com>
2020-03-02 00:04:21 -0500

    composes

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable react/no-did-mount-set-state */
 import React from 'react';
-import styles from '../styles/pages/App.module.css';
+import styles from './App.module.css';
 
 const App = () => {
   console.log(styles);
   return (
     <div className={`${styles.container}`}>
-    <p>hola</p>
+    <p className={styles.btn__inicio}>hola</p>
     <p>hola</p>
     <p>hola</p>
     <p>hola</p>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './containers/App';
+import App from './containers/App/App';
 
 const app = document.getElementById('app');
 ReactDOM.render(<App />,
