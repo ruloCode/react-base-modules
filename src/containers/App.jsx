a28a190cbd2e4f2372396df27fc60d16f7d65e11
Andrés <drew.lizcano@gmail.com>
2020-03-01 21:04:44 -0500

    simple

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,60 +1,13 @@
 /* eslint-disable react/no-did-mount-set-state */
 import React from 'react';
-import '../styles/components/App.styl';
-import Header from '../components/Header';
-import About from '../components/About';
-import Profile from '../components/Profile';
-import Experience from '../components/Experience';
-import Academic from '../components/Academic';
-import Skills from '../components/Skills';
-import Interest from '../components/Interest';
-import Languages from '../components/Languages';
-import Social from '../components/Social';
+import '../styles/pages/App.styl';
 
-import getData from '../utils/getData';
 
-class App extends React.Component {
-  // const [profile, setProfile] = useState([]);
-  // const data = getData();
-  // const handleGetData = () => {
-  //   setProfile(data);
-  // };
-  constructor(props) {
-    super(props);
-    this.state = {
-      profile: [],
-    };
-  }
+const App = ()=>{
+  return(
+    <div>Hello react</div>
+  )
+}
 
-  async componentDidMount() {
-    const data = await getData();
-    this.setState({
-      profile: data,
-    });
-  }
 
-  render() {
-    const { profile } = this.state;
-    return (
-      <div className='App'>
-        <Header {...profile}>
-          <About {...profile} />
-        </Header>
-        <div className='AppContainer'>
-          <Profile className='boxItem' {...profile} />
-          <Experience {...profile} />
-          <Academic {...profile} />
-          <Skills {...profile} />
-          <Interest {...profile} />
-          <Languages {...profile} />
-        </div>
-
-        <div>
-          <Social {...profile} />
-        </div>
-
-      </div>
-    );
-  }
-};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 /* eslint-disable react/jsx-indent */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import reduxThunk from 'redux-thunk';
-import App from './containers/App';
-import reducer from './reducers/profileReducers';
 
-const store = createStore(
-  reducer,
-  {},
-  composeWithDevTools(
-    applyMiddleware(reduxThunk),
-  ),
-);
+import App from './containers/App';
+
+
 const app = document.getElementById('app');
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+ReactDOM.render( <
+  App / > ,
   app,
 );
